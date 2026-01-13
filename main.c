414a1f829f7927d19862847e4aac0ea940dbfc66
KaMMon_ <142399721+KaMMon4ik@users.noreply.github.com>
2026-01-13 11:11:03 +0300

    Файл с функциями, которые проходят все тесты

---
+++ main.c
@@ -0,0 +1,160 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "test.h"
+
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <memory.h>
+
+double* full_elements(double* ptr, int len) {
+
+	if (ptr==NULL || len<=0) return NULL;
+	double* new_ptr = malloc(len*sizeof(double));
+	if (new_ptr==NULL) return NULL;
+
+	for (int i=0; i<len; ++i) new_ptr[i] = pow(i+1, 2) + sin(5*(i+1));
+
+	return new_ptr;
+
+}
+
+double* calc_elements(double *ptr, int len) {
+
+	if (ptr==NULL || len<=0) return NULL;
+	double* new_ptr = malloc(len*sizeof(double));
+	if (new_ptr==NULL) return NULL;
+
+	for (int i=0; i<len; ++i) new_ptr[i] = ptr[i] * 2;
+
+	return new_ptr;
+
+}
+
+void put_elements(double* ptr, int len) {
+
+	if (ptr==NULL || len<=0) return NULL;
+
+	for (int i=0; i<len; ++i) printf("%lf ", ptr[i]);
+	printf("\n");
+
+}
+
+double* insert_element(double* ptr, int len, int index, double val) {
+
+	if (ptr==NULL || len<=0) return NULL;
+    double* new_ptr = malloc((len+1)*sizeof(double));
+	if (new_ptr==NULL) return NULL;
+
+	if (index<0 || index>len) return NULL;
+
+    for (int i=0; i<index; ++i) new_ptr[i] = ptr[i];
+    new_ptr[index] = val;
+    for (int i=index; i<len; ++i) new_ptr[i+1] = ptr[i];
+
+    return new_ptr;
+}
+
+double* delete_element(double* ptr, int len, int index) {
+
+	if (ptr==NULL || len<=0) return NULL;
+	double* new_ptr = malloc((len-1)*sizeof(double));
+	if (new_ptr==NULL) return NULL;
+
+	if (index<0 || index>=len) return NULL;
+
+	for (int i=0; i<index; ++i) new_ptr[i] = ptr[i];
+	for (int i=index; i<(len-1); ++i) new_ptr[i] = ptr[i+1];
+
+    return new_ptr;
+
+}
+
+int comparator(double* a, double* b) {
+	if (*a<*b) return -1;
+	if (*a>*b) return 1;
+	return 0;
+}
+
+double* sort_array(double* ptr, int len) {
+
+	if (ptr==NULL || len<=0) return NULL;
+	double* new_ptr = malloc(len*sizeof(double));
+	if (new_ptr==NULL ) return NULL;
+
+	memcpy(new_ptr, ptr, len*sizeof(double));
+	
+	qsort(new_ptr, len, sizeof(double), comparator);
+	return new_ptr;
+
+}
+
+double sum_elements(double* ptr, int len, int begin, int end) {
+
+	if (ptr==NULL || len<=0 || begin<0 || begin>=len || end<0 || end>=len || begin>end) return 0;
+
+	double sum = 0;
+	for (int i=begin; i<=end; ++i) sum += ptr[i];
+
+	return sum;
+
+}
+
+int find_element(double* ptr, int len, double val) {
+
+	if (ptr==NULL || len<=0) return -1;
+
+	int index = -1;
+	for (int i=0; i<len; ++i) {
+		if (ptr[i]==val) {
+			index = i;
+			break;
+		}
+	}
+
+	return index;
+
+}
+
+double* task_1() {
+
+	double* ptr;
+	int len;
+
+	puts("¬ведите длину массива: ");
+	scanf("%d", &len);
+
+	if (len<=0) return NULL;
+	ptr = (double*) malloc(len*sizeof(double));
+	if (ptr==NULL) return NULL;
+
+	ptr = full_elements(ptr, len);
+	put_elements(ptr, len);
+
+	ptr = calc_elements(ptr, len);
+	put_elements(ptr, len);
+
+	free(ptr);
+
+	return ptr;
+
+}
+
+void main() {
+
+	setlocale(LC_ALL, "RUS");
+
+	test_task_1(task_1);
+	
+	test_insert_element(insert_element);
+
+	test_delete_element(delete_element);
+
+	test_sort_array(sort_array);
+
+	test_sum_elements(sum_elements);
+
+	test_find_element(find_element);
+
+}
